fe14e1c2a890c1453c2cdc02e19e6718e01c0fce
scruffy1488 <58042996+scruffy1488@users.noreply.github.com>
2021-01-08 23:01:44 -0600

    update

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -22,7 +22,7 @@ var l_displayMapGamemode = true;
 
 // Custom server name configuration
 // Leave it blank for automatic server name
-var l_serverName = "Runic Networks | First Order RP";
+var l_serverName = "Runic Networks First Order";
 
 // Use a video for the background?
 // Otherwise it will use images
